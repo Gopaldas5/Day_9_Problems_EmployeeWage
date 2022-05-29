e10a6cb8a51cf5389030830d10df78848fc22bec
GopalDas <gopalkrdas1997@gmail.com>
2022-05-30 00:32:21 +0530

    ADD: calculating the wage for month

--- src/com/bridgeLabz/EmployeeWage9.java
+++ src/com/bridgeLabz/EmployeeWage9.java
@@ -7,22 +7,27 @@ public class EmployeeWage9 {
     private static final double IS_PART_TIME = 1;
     public static  final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 2;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-
-
-        Random random = new Random();
-        int employeeCheck = random.nextInt(2);
         int empHrs = 0;
         int empWage = 0;
+        int totalEmpWage = 0;
 
-        empHrs = switch (employeeCheck) {
-            case (int) IS_PART_TIME -> 4;
-            case IS_FULL_TIME -> 8;
-            default -> 0;
-        };
-        empWage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Employee wage : "+empWage);;
+        Random random = new Random();
+
+        for (int day=0; day<NUM_OF_WORKING_DAYS; day++) {
+            int employeeCheck = random.nextInt(2);
+            empHrs = switch (employeeCheck) {
+                case (int) IS_PART_TIME -> 4;
+                case IS_FULL_TIME -> 8;
+                default -> 0;
+            };
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            totalEmpWage += empWage;
+            System.out.println("Employee wage : " + empWage);
+        }
+        System.out.println("Total Employee wage : "+totalEmpWage);
         }
     }
 
