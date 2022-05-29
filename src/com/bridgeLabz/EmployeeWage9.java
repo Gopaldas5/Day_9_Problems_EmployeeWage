cd4039e57b39d783ed73c5e338e27f12ff400e7d
GopalDas <gopalkrdas1997@gmail.com>
2022-05-29 23:54:39 +0530

    ADD: Displaying Welcome page

---
+++ src/com/bridgeLabz/EmployeeWage9.java
@@ -0,0 +1,7 @@
+package com.bridgeLabz;
+
+public class EmployeeWage9 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+    }
+}
